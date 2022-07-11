b23ace48695dbd5ade1885a43ac2ecebeafe151c
Ryan Yang <ryang1453@gmail.com>
2022-07-11 00:30:02 -0400

    sdk: better vm run script

--- run_sdk_vm.sh
+++ run_sdk_vm.sh
@@ -1,24 +1,76 @@
 #!/usr/bin/env bash
 set -e
 
-cd "$(dirname "$0")"
-
 # Prereqs
+# - x86_64 host
 # - KVM Virtualuzation available (scripts/kvm_ok)
 # - Docker
 
-# TODO: Remove dependency from docker/docker.io
-#docker run -it --rm \
-#            --device /dev/kvm \
-#            -v "$(pwd)":/homeworld \
-#            docker.io/nixos/nix \
-#            /bin/sh /homeworld/scripts/setup_vm.sh
+cd "$(dirname "$0")"
 
+containsElement () {
+  # args: needle haystack
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+# Grab all available guests from the hosts/ dir
+guests=(hosts/*/)
+guests=("${guests[@]%/}")     # Remove trailing slash
+guests=("${guests[@]##*/}")   # Remove path prefixes
+
+# Check if specified VM exists as a guest configuration
+if ! containsElement "$1" "${guests[@]}" ; then
+  echo "Requested configuration not found: $1" 1>&2
+  echo "Usage: run_sdk_vm.sh <configuration> [qemu_args]"
+  echo "Available Configurations:"
+  printf '* %s\n' "${guests[@]}"
+  exit 1
+fi
+
+# Save VM locations
+HOMEWORLD_PATH=$(realpath .)
+HOMEWORLD_VM_ROOT=${HOMEWORLD_VM_ROOT:-"${HOMEWORLD_PATH}/vm"}
+VM_GUEST="$1"
+VM_FILE="${HOMEWORLD_VM_ROOT}/$1.qcow2"
+VM_SOCKET="${HOMEWORLD_VM_ROOT}/$1.socket"
+
+echo "Running VM: ${VM_FILE}"
+
+if [ ! -f "${VM_FILE}" ]; then
+    echo "VM Disk ${VM_FILE} does not exist yet, creating..."
+
+    # TODO: Remove dependency from docker/docker.io
+    docker run -it --rm \
+                --device /dev/kvm \
+                -v "$HOMEWORLD_PATH":/homeworld \
+                docker.io/nixos/nix \
+                /bin/sh /homeworld/scripts/setup_vm.sh "${VM_GUEST}"
+
+    echo "VM created. Moving to HOMEWORLD_VM_ROOT"
+    mkdir -p "${HOMEWORLD_VM_ROOT}"
+    mv "${VM_FILE}" "${HOMEWORLD_VM_ROOT}"
+fi
+
+# Grab VM-specific arguments
+mapfile -t <"hosts/${VM_GUEST}/qemu.args"
+
+# Show the qemu command.
+# NOTE: To perform a dry run, pass in invalid arguments in the command line, such as `-bios NONE`
 set -o xtrace
-qemu-system-x86_64 -enable-kvm -snapshot -nographic \
+
+# Wrapping the last two arguments in quotes removes the whitespace between args that we want to maintain.
+# NOTE: We may want to research looking into using the -fw_cfg argument to pass files into /sys/firmware/qemu-fw-cfg
+qemu-system-x86_64 -enable-kvm -nographic \
     -cpu host \
-    -m 4096 \
-    -nic user,model=virtio,hostfwd=tcp::2222-:22 \
-    -drive if=virtio,file=hosts/qemu_vm/nixos.qcow2 \
-    -virtfs local,path="$(pwd)",mount_tag=host0,security_model=none,id=host0,readonly \
-    -monitor unix:/tmp/qemu-monitor-nixos.socket,server,nowait
+    -m 2048 \
+    -drive if=virtio,file="${VM_FILE}" \
+    -virtfs local,path="${HOMEWORLD_PATH}",mount_tag=host0,security_model=none,id=host0,readonly=on \
+    -serial mon:stdio \
+    -monitor unix:"${VM_SOCKET}",server,nowait ${MAPFILE[@]} ${@:2}
+
+# TODO: Research solution to this
+# After finishing the script, may need to run 'tput smam' to fix text wrapping
+# (https://bugs.launchpad.net/qemu/+bug/1857449)
--- scripts/setup_vm.sh
+++ scripts/setup_vm.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/env bash
 set -e
+set -o xtrace
+
+# Params:
+# $1: configuration name (hosts folder name)
 
 # TODO: Migrate to nix flakes once promoted from experimental to stable
 
@@ -12,5 +16,5 @@ nix-channel --update
 # Install nixos-generators: https://github.com/nix-community/nixos-generators
 nix-env -f https://github.com/nix-community/nixos-generators/archive/master.tar.gz -i
 
-nixos-generate --configuration /homeworld/hosts/qemu_vm/configuration.nix  --system x86_64-linux --format qcow --out-link workdir
-cp workdir/nixos.qcow2 /homeworld/hosts/qemu_vm
+nixos-generate --configuration "/homeworld/hosts/$1/configuration.nix"  --system x86_64-linux --format qcow --out-link workdir -I
+cp workdir/nixos.qcow2 "/homeworld/$1.qcow2"
